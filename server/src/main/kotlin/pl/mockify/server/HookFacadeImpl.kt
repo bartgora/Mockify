d63cb824b36f47c69e4bf941d135ff92afaf849d
bartlomiej-gora <bartlomiej.gora@gmail.com>
2021-11-14 22:30:57 +0100

    Added HTTP Method to requests

--- server/src/main/kotlin/pl/mockify/server/HookFacadeImpl.kt
+++ server/src/main/kotlin/pl/mockify/server/HookFacadeImpl.kt
@@ -13,29 +13,39 @@ class HookFacadeImpl(private val hookService: HookService) : HookFacade {
         method: HttpMethod
     ) {
         when (method) {
-            HttpMethod.GET -> processGet(name, body, headers)
-            HttpMethod.POST -> processPost(name,body, headers)
+            HttpMethod.GET, HttpMethod.DELETE -> processEmptyRequestBody(name, body, headers, method)
+            HttpMethod.POST, HttpMethod.PUT, HttpMethod.PATCH -> processWithRequestBody(name, body, headers, method)
+            else -> null;
         }
-
     }
 
-    private fun processPost(name: String, body: Map<String, String>?, headers: Map<String, String>) {
-        var exitingHook = hookService.getHook(name)
+    private fun processWithRequestBody(
+        name: String,
+        body: Map<String, String>?,
+        headers: Map<String, String>,
+        method: HttpMethod
+    ) {
+        val exitingHook = hookService.getHook(name)
         if (exitingHook == null) {
             throw IllegalStateException("No hook!")
         } else {
-            exitingHook.requests = exitingHook.requests.plus(createRequest(HttpMethod.POST, body, headers))
+            exitingHook.requests = exitingHook.requests.plus(createRequest(method, body, headers))
             hookService.saveHook(exitingHook)
 
         }
     }
 
-    private fun processGet(name: String, body: Map<String, String>?, headers: Map<String, String>) {
-        var exitingHook = hookService.getHook(name)
+    private fun processEmptyRequestBody(
+        name: String,
+        body: Map<String, String>?,
+        headers: Map<String, String>,
+        method: HttpMethod
+    ) {
+        val exitingHook = hookService.getHook(name)
         if (exitingHook == null) {
             hookService.saveHook(createNeHook(name, body, headers, HttpMethod.GET))
         } else {
-            exitingHook.requests = exitingHook.requests.plus(createRequest(HttpMethod.GET, body, headers))
+            exitingHook.requests = exitingHook.requests.plus(createRequest(method, body, headers))
             hookService.saveHook(exitingHook)
 
         }
@@ -46,12 +56,17 @@ class HookFacadeImpl(private val hookService: HookService) : HookFacade {
         return Request(method, body, headers)
     }
 
-    private fun createNeHook(name: String, body: Map<String, String>?, headers: Map<String, String>, method: HttpMethod): Hook {
-        var request = Request(method, body, headers)
+    private fun createNeHook(
+        name: String,
+        body: Map<String, String>?,
+        headers: Map<String, String>,
+        method: HttpMethod
+    ): Hook {
+        val request = Request(method, body, headers)
         return Hook(name, listOf(request))
     }
 
     override fun getResponses(name: String): List<Request> {
-        return hookService.getHook(name)!!.requests;
+        return hookService.getHook(name)!!.requests
     }
 }
--- server/src/main/kotlin/pl/mockify/server/WebhookController.kt
+++ server/src/main/kotlin/pl/mockify/server/WebhookController.kt
@@ -2,24 +2,31 @@ package pl.mockify.server
 
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
+import org.springframework.http.server.ServletServerHttpRequest
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 class WebhookController(private var hookFacade: HookFacade) {
 
 
-    @GetMapping("/hook/{name}")
-    fun getWebhook(@PathVariable name: String,
-                      @RequestHeader headers : Map<String, String>): ResponseEntity<List<Request>> {
+    @RequestMapping("/hook/{name}", method = [RequestMethod.GET, RequestMethod.DELETE])
+    fun getWebhook(
+        @PathVariable name: String,
+        @RequestHeader headers: Map<String, String>
+    ): ResponseEntity<List<Request>> {
         hookFacade.processRequest(name, null, headers, HttpMethod.GET)
         return ResponseEntity.ok().body(hookFacade.getResponses(name))
     }
 
-    @PostMapping("/hook/{name}")
-    fun postWebhook(@PathVariable name: String,
-                    @RequestBody body : Map<String, String>,
-                      @RequestHeader headers : Map<String, String>): ResponseEntity<List<Request>> {
-        hookFacade.processRequest(name, body, headers, HttpMethod.POST)
+    @RequestMapping("/hook/{name}", method = [RequestMethod.POST, RequestMethod.PATCH, RequestMethod.PUT])
+    fun postWebhook(
+        @PathVariable name: String,
+        @RequestBody body: Map<String, String>,
+        @RequestHeader headers: Map<String, String>,
+        servletRequest: HttpServletRequest
+    ): ResponseEntity<List<Request>> {
+        hookFacade.processRequest(name, body, headers, HttpMethod.valueOf(servletRequest.method))
         return ResponseEntity.ok().body(hookFacade.getResponses(name))
     }
 }
