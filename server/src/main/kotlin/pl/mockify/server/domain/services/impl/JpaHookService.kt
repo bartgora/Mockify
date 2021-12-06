a0d2573fea68de6b5524a6380dc545262aabbc18
bgora <bartlomiej.gora@infobip.com>
2021-12-06 10:42:40 +0100

    Reactive

--- server/src/main/kotlin/pl/mockify/server/domain/services/impl/JpaHookService.kt
+++ server/src/main/kotlin/pl/mockify/server/domain/services/impl/JpaHookService.kt
@@ -1,5 +1,6 @@
 package pl.mockify.server.domain.services.impl
 
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 import pl.mockify.server.data.HookRepository
 import pl.mockify.server.domain.Hook
@@ -12,14 +13,16 @@ import java.time.LocalDateTime
 class JpaHookService(private var hookRepository: HookRepository) : HookService {
 
     override suspend fun saveHook(hook: Hook): Hook {
-        val existingHook = hookRepository.getByName(hook.name)
-        if (existingHook != null) {
-            existingHook.lastModified = Timestamp.valueOf(LocalDateTime.now())
-            existingHook.responseTemplate = bodyToString(hook.responseTemplate.body)
-            existingHook.events = existingHook.events.plus(convertEventToDB(hook.events.last()))
-            return convertHookFromDB(hookRepository.save(existingHook))
+       return runBlocking {
+            val existingHook =  hookRepository.getByName(hook.name)
+            if (existingHook != null) {
+                existingHook.lastModified = Timestamp.valueOf(LocalDateTime.now())
+                existingHook.responseTemplate = bodyToString(hook.responseTemplate.body)
+                existingHook.events = existingHook.events.plus(convertEventToDB(hook.events.last()))
+                return@runBlocking convertHookFromDB(hookRepository.save(existingHook))
+            }
+            return@runBlocking convertHookFromDB(hookRepository.save(convertHookToDB(hook)))
         }
-        return convertHookFromDB(hookRepository.save(convertHookToDB(hook)))
     }
 
     override suspend fun getHook(customName: String): Hook? {
