a6139e314cb69d419ca2f2c4b6d7133db4c8ade9
bartlomiej-gora <bartlomiej.gora@gmail.com>
2022-02-18 20:51:16 +0100

    Changed Cron

--- server/src/main/kotlin/pl/mockify/server/data/ExpireHookCleaner.kt
+++ server/src/main/kotlin/pl/mockify/server/data/ExpireHookCleaner.kt
@@ -1,6 +1,7 @@
 package pl.mockify.server.data
 
 import mu.KotlinLogging
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import java.sql.Timestamp
@@ -11,11 +12,11 @@ open class ExpireHookCleaner(private var hookRepository: HookRepository) {
 
     private var logger = KotlinLogging.logger("ExpireHookCleaner")
 
-//    @Scheduled(fixedDelay = 1800000)
+    @Scheduled(cron = "0/1 0/15 0 ? * * *")
     @Transactional
     open fun execute() {
         logger.info { "Executing Scheduler" }
-        val timeStamp = Timestamp.valueOf(LocalDateTime.now().minusHours(4))
+        val timeStamp = Timestamp.valueOf(LocalDateTime.now().minusHours(8))
         val hooks = hookRepository.findByLastModifiedBefore(timeStamp)
         logger.info { "cleaning ${hooks.size} hooks" }
         hookRepository.deleteAll(hooks)
