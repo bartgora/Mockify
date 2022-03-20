fe3b9f9b99f62f3b624e18d797a01c0627df3754
bartlomiej-gora <bartlomiej.gora@gmail.com>
2022-03-20 09:11:02 +0100

    Removed some interfaces, refactored, and eformatteed some code

--- frontend/mockify-ui/src/components/EventPanel.tsx
+++ frontend/mockify-ui/src/components/EventPanel.tsx
@@ -6,13 +6,14 @@ interface Props {
 }
 class EventPanel extends React.Component<Props, Props> {
   render(): React.ReactNode {
+    const content = JSON.stringify(this.props.event.response.body);
     return (
       <div className="item">
         <div className="content">
           <div className="header">
             Method: {this.props.event.request.method}, Date: {this.props.event.timestamp}
           </div>
-          <div className="content">{JSON.stringify(this.props.event.response.body)}</div>
+          <div className="content">{content}</div>
           <div className="ui divider"></div>
         </div>
       </div>
