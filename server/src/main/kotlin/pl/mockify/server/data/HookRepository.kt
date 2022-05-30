42ee713222adfc6ea12fb27fa4a91a7303434ccc
bartlomiej-gora <bartlomiej.gora@gmail.com>
2022-05-30 21:04:31 +0200

    Added Test containers, checked if removal of eager worked

--- server/src/main/kotlin/pl/mockify/server/data/HookRepository.kt
+++ server/src/main/kotlin/pl/mockify/server/data/HookRepository.kt
@@ -2,13 +2,11 @@ package pl.mockify.server.data
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.sql.Timestamp
 import javax.transaction.Transactional
 
 @Repository
 interface HookRepository : JpaRepository<Hook, Long> {
 
-    fun findByLastModifiedBefore(lastModified: Timestamp): List<Hook>
 
     fun findByName(name: String): Hook?
 }
