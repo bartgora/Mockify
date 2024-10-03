8675c0d9a43b6559bf01347f874d7d420ba51877
Góra Bartłomiej <Bartlomiej.Gora@cyfra.gov.pl>
2024-10-03 15:55:40 +0200

    update versions + openapi.yaml

--- server/src/main/kotlin/pl/mockify/server/domain/facades/HookFacade.kt
+++ server/src/main/kotlin/pl/mockify/server/domain/facades/HookFacade.kt
@@ -1,6 +1,9 @@
 package pl.mockify.server.domain.facades
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import pl.mockify.server.domain.Event
@@ -39,20 +42,25 @@ class HookFacade(private val hookService: HookService, private val hookFactory: 
     }
 
     suspend fun updateResponse(name: String, body: Map<String, String>): Response {
-        val hook = withContext(Dispatchers.IO) {
-            hookService.getHook(name)
-        } ?: throw IllegalStateException("No Hook!")
-        val newResponse = Response(body)
-        hook.responseTemplate = newResponse
-        withContext(Dispatchers.IO) {
-            hookService.updateResponse(hook)
-        }
+        return coroutineScope {
+            val hookDeferred = async {
+                withContext(Dispatchers.IO) {
+                    hookService.getHook(name)
+                } ?: throw IllegalStateException("No Hook!")
+            }
+            val newResponse = Response(body)
+            val hook = hookDeferred.await()
+            hook.responseTemplate = newResponse
+            withContext(Dispatchers.IO) {
+                hookService.updateResponse(hook)
+            }
 
-        return newResponse
+            return@coroutineScope newResponse
+        }
     }
 
     suspend fun getEvents(name: String): List<Event> {
-        val hook = runBlocking {
+        val hook = coroutineScope {
             async(Dispatchers.IO) {
                 hookService.getHook(name) ?: throw IllegalStateException("No Hook!")
             }
@@ -62,7 +70,7 @@ class HookFacade(private val hookService: HookService, private val hookFactory: 
     }
 
     suspend fun deleteEvents(name: String) {
-        runBlocking {
+        coroutineScope {
             val hook = async(Dispatchers.IO) {
                 hookService.getHook(name) ?: throw IllegalStateException("No Hook!")
             }
@@ -79,19 +87,28 @@ class HookFacade(private val hookService: HookService, private val hookFactory: 
         headers: Map<String, String>,
         method: HttpMethod
     ): Response {
-        val existingHookDeferred =
-            runBlocking { async(Dispatchers.IO) { hookService.getHook(name) } }
-        val exitingHook = existingHookDeferred.await()
-        if (exitingHook == null) {
-            throw IllegalStateException("No hook!")
-        } else {
-            exitingHook.addEvent(createEvent(method, body, headers, exitingHook.responseTemplate))
-            withContext(Dispatchers.IO) {
-                hookService.saveHook(exitingHook)
-            }
-        }
 
-        return exitingHook.events.map { event -> event.response }.last()
+        return coroutineScope {
+            val existingHookDeferred = async(Dispatchers.IO) { hookService.getHook(name) }
+            val exitingHook = existingHookDeferred.await()
+            if (exitingHook == null) {
+                throw IllegalStateException("No hook!")
+            } else {
+                exitingHook.addEvent(
+                    createEvent(
+                        method,
+                        body,
+                        headers,
+                        exitingHook.responseTemplate
+                    )
+                )
+                withContext(Dispatchers.IO) {
+                    hookService.saveHook(exitingHook)
+                }
+            }
+
+            return@coroutineScope exitingHook.events.map { event -> event.response }.last()
+        }
     }
 
     private suspend fun createEvent(
